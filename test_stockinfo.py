b28ea000444cba63e486de98cd1fa29fac7518b6
atran1015 <aichantran1015@yahoo.com>
2022-07-15 14:23:36 -0700

    add comment

--- test_stockinfo.py
+++ test_stockinfo.py
@@ -1,12 +1,12 @@
 import unittest
 import yfinance as yf
-
+import stockinfo
  
 class SelectStock(unittest.TestCase):
  
     def testDownload(self):
         self.assertNotEqual(yf,'None')
-        
+    # test function only, check to see if it returns accurate results or not
  
 if __name__ == '__main__':
     unittest.main()
