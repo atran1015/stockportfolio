ff80aa3c99e1626b753b811ebc22e08bc6a45d09
atran1015 <aichantran1015@yahoo.com>
2022-07-16 15:51:17 -0700

    update code and remove comments

--- stockinfo.py
+++ stockinfo.py
@@ -5,18 +5,18 @@ import pandas as pd
 # print("This is a downloading the data:")
 # data = yf.download("DOW", start="2022-06-15", end="2022-07-14")
 # print(data)
-def GetStock(dow):
+def GetStock(ticker):
     print("\nThis is saving data to a variable:")
     
     # get stock info
-    dow.info
+    ticker.info
 
     # get historical market data
-    hist = dow.history(period="1mo")
+    hist = ticker.history(period="1mo")
 
     # print("show analyst recommendations:")
     # print(dow.recommendations)
-    analyst_recdf = pd.DataFrame(dow.recommendations)
+    analyst_recdf = pd.DataFrame(ticker.recommendations)
 
     df = pd.DataFrame(hist)
     # print(type(df)) - for debug
@@ -29,15 +29,12 @@ def GetStock(dow):
     df3 = pd.concat([df2, analyst_recdf])
     print(df3)
 
-
-    # TODO: display ask, bid, PE Ratios, EPS, analyst recommendations
-    # comment: i'm not sure if we need to print data over a certain period
-    # the print statements below only print the current data
-    print("two PE ratios are trailingPE and forwardPE: ", dow.info["trailingPE"], dow.info["forwardPE"])
-    print("ask: ", dow.info["ask"])
-    print("bid: ", dow.info["bid"])
-    print("earnings/revenue per share (EPS?): ", dow.info["revenuePerShare"])
+    print("two PE ratios are trailingPE and forwardPE: ", ticker.info["trailingPE"], ticker.info["forwardPE"])
+    print("ask: ", ticker.info["ask"])
+    print("bid: ", ticker.info["bid"])
+    print("EPS: ", ticker.info["revenuePerShare"])
 
 
-dow = yf.Ticker("DOW")
-GetStock(dow)
+inp = input('Input a stock ticker: ')
+stock = yf.Ticker(inp)
+GetStock(stock)
