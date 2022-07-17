585fba292df79777b5725b50c9d8c9fea910173c
atran1015 <aichantran1015@yahoo.com>
2022-07-17 04:55:02 -0700

    completed test_stockinfo

--- stockinfo.py
+++ stockinfo.py
@@ -5,36 +5,33 @@ import pandas as pd
 # print("This is a downloading the data:")
 # data = yf.download("DOW", start="2022-06-15", end="2022-07-14")
 # print(data)
-def GetStock(ticker):
-    print("\nThis is saving data to a variable:")
-    
-    # get stock info
-    ticker.info
 
-    # get historical market data
-    hist = ticker.history(period="1mo")
+def PrintHistoricalAndRec(ticker):
+        hist = ticker.history(period="1mo")
+        analyst_recdf = pd.DataFrame(ticker.recommendations)
+        df = pd.DataFrame(hist)
+        df2 = df[["Open","Volume","Close","Low"]]
+        df3 = pd.concat([df2, analyst_recdf])
+        print(df3)
 
-    # print("show analyst recommendations:")
-    # print(dow.recommendations)
-    analyst_recdf = pd.DataFrame(ticker.recommendations)
+def PrintPERatios(ticker):
+        print("two PE ratios are trailingPE and forwardPE: ", ticker.info["trailingPE"], ticker.info["forwardPE"])
 
-    df = pd.DataFrame(hist)
-    # print(type(df)) - for debug
-    column_headers = list(df.columns.values)
-    # print("The Column Header :", column_headers) - for debug
-    df2 = df[["Open","Volume","Close","Low"]]
-    # print(df2)
+def PrintAsk(ticker):
+        print("ask: ", ticker.info["ask"])
 
-    # appending analyst recommendations columns to selected historical columns
-    df3 = pd.concat([df2, analyst_recdf])
-    print(df3)
+def PrintBid(ticker):
+        print("bid: ", ticker.info["bid"])
 
-    print("two PE ratios are trailingPE and forwardPE: ", ticker.info["trailingPE"], ticker.info["forwardPE"])
-    print("ask: ", ticker.info["ask"])
-    print("bid: ", ticker.info["bid"])
-    print("EPS: ", ticker.info["revenuePerShare"])
-
+def PrintEPS(ticker):
+        print("EPS: ", ticker.info["revenuePerShare"])
 
 inp = input('Input a stock ticker: ')
 stock = yf.Ticker(inp)
-GetStock(stock)
+
+
+PrintHistoricalAndRec(stock)
+PrintPERatios(stock)
+PrintAsk(stock)
+PrintBid(stock)
+PrintEPS(stock)
--- test_stockinfo.py
+++ test_stockinfo.py
@@ -1,12 +1,58 @@
 import unittest
+#from unittest.mock import patch
+import io
+import sys
 import yfinance as yf
-import stockinfo
- 
+from stockinfo import *
+
+# test function only, check to see if it returns accurate results or not 
+
 class SelectStock(unittest.TestCase):
- 
-    def testDownload(self):
-        self.assertNotEqual(yf,'None')
-    # test function only, check to see if it returns accurate results or not
+
+    @staticmethod
+    def testPrintHistoricalAndRec():
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        stock = yf.Ticker("DOW")
+        PrintHistoricalAndRec(stock)
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        print ('Captured', capturedOutput.getvalue())   # Now works as before.
+
+    @staticmethod
+    def testPrintPERatios():
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        stock = yf.Ticker("DOW")
+        PrintPERatios(stock)
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        print ('Captured', capturedOutput.getvalue())   # Now works as before.
+    
+    @staticmethod
+    def testPrintAsk():
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        stock = yf.Ticker("DOW")
+        PrintAsk(stock)
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        print ('Captured', capturedOutput.getvalue())   # Now works as before.
+
+    @staticmethod
+    def testPrintBid():
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        stock = yf.Ticker("DOW")
+        PrintBid(stock)
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        print ('Captured', capturedOutput.getvalue())   # Now works as before.
+
+    @staticmethod
+    def testPrintEPS():
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        stock = yf.Ticker("DOW")
+        PrintEPS(stock)
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        print ('Captured', capturedOutput.getvalue())   # Now works as before.
  
 if __name__ == '__main__':
     unittest.main()
